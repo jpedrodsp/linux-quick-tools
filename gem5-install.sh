e43707e8112d82c3c66f72ecd090080695455d96
Joao Pedro Patrocinio <jpedro9966@gmail.com>
2020-07-09 23:22:47 -0300

    Added gem5-riscv installation script.

---
+++ gem5-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+script_id="gem5install"
+gem5_repository="https://gem5.googlesource.com/public/gem5"
+actual_dir=$(pwd)
+
+
+# Write install code here
+echo -e "Installing pre-requisites..."
+sudo apt install -y build-essential git m4 scons zlib1g zlib1g-dev libprotobuf-dev protobuf-compiler libprotoc-dev libgoogle-perftools-dev python-dev python
+sudo apt install -y git
+sudo apt install -y build-essential
+sudo apt install -y scons
+sudo apt install -y python-dev
+sudo apt install -y libprotobuf-dev python-protobuf protobuf-compiler libgoogle-perftools-dev
+sudo apt install -y libboost-all-dev
+sudo apt install -y automake
+
+echo -e "Cloning gem5 repository and building a RISC-V optimized binary..."
+git clone https://gem5.googlesource.com/public/gem5
+cd ./gem5
+scons build/RISCV/gem5.opt -j2
+
+cd $actual_dir
+echo -e "Done!"
