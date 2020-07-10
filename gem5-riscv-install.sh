889f337afd6fc6699c5707251033c8734f4b8fc0
Joao Pedro Patrocinio <jpedro9966@gmail.com>
2020-07-09 23:25:12 -0300

    Added automatic CPU count get in gem5-riscv-install.

--- gem5-riscv-install.sh
+++ gem5-riscv-install.sh
@@ -3,7 +3,8 @@
 script_id="gem5-riscv-install"
 gem5_repository="https://gem5.googlesource.com/public/gem5"
 actual_dir=$(pwd)
-
+cpu_count=$(grep -c ^processor /proc/cpuinfo)
+echo -e "Got CPU count: $cpu_count"
 
 # Write install code here
 echo -e "Installing pre-requisites..."
@@ -19,7 +20,7 @@ sudo apt install -y automake
 echo -e "Cloning gem5 repository and building a RISC-V optimized binary..."
 git clone https://gem5.googlesource.com/public/gem5
 cd ./gem5
-scons build/RISCV/gem5.opt -j2
+scons build/RISCV/gem5.opt -j$cpu_count
 
 cd $actual_dir
 echo -e "Done!"
