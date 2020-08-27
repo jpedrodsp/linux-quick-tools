6f157f6773c0b78fdecafd3ea6a4b31b43aaf328
Joao Pedro Patrocinio <jpedro9966@gmail.com>
2020-08-26 21:33:27 -0300

    Added TP-Link AC600 USB wireless device support

---
+++ wireless-driver-tplink-ac600-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+script_id="ac600-setup"
+
+# Install needed package
+sudo apt update -y
+sudo apt install -y git bc build-essential dkms
+
+# Set-up temporary directory
+previous_dir=$(pwd)
+temporary_directory="$(pwd)/.$script_id-temp"
+echo -e "Setting up temporary directory: $temporary_directory"
+mkdir $temporary_directory
+
+# Write install code here
+echo -e "Downloading TP-Link AC600 driver [2357:011e] ..."
+git clone https://github.com/aircrack-ng/rtl8812au.git $temporary_directory/ac600driver
+echo -e "Installing TP-Link AC600 driver [2357:011e] ..."
+cd $temporary_directory/ac600driver
+sudo ./dkms-install.sh
+cd $previous_dir
+
+# Remove temporary directory
+echo -e "Removing temporary directory: $temporary_directory"
+rm -rf $temporary_directory
