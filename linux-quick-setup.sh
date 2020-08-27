f05877b357fe8f3b066a3bfc523f7e36f991e864
Joao Pedro Patrocinio <jpedro9966@gmail.com>
2020-08-26 22:02:22 -0300

    Added apt-fast bash autocomplete install

--- linux-quick-setup.sh
+++ linux-quick-setup.sh
@@ -27,6 +27,14 @@ sudo apt install -y -f
 cd $tmp_localdir
 rm -Rf $lqs_tempinstalldir
 
+# Install apt-fast autocompletion
+git clone https://github.com/ilikenwf/apt-fast.git apt-fast
+sudo apt install -y bash-completion
+sudo cp apt-fast/completions/bash/apt-fast /etc/bash_completion.d/
+sudo chown root:root /etc/bash_completion.d/apt-fast
+. /etc/bash_completion
+sudo rm -rf apt-fast
+
 # Install Youtube-Dl via pip3 + FFMPEG
 sudo apt-fast install -y python3 python3-pip ffmpeg
 sudo pip3 install youtube-dl
