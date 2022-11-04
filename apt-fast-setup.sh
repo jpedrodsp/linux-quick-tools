ac8e36aa862b7132a0bc3a787ab58c1f6e4bb830
João Pedro Patrocinio <jpedro9966@gmail.com>
2022-11-04 13:59:35 -0300

    Make constants uppercase; Fix install order for 'git' package;

--- apt-fast-setup.sh
+++ apt-fast-setup.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # Create temporary directory
-tmp_localdir=$(pwd)
-lqs_tempinstalldir=".aptfast-setup"
-mkdir $lqs_tempinstalldir
-cd $lqs_tempinstalldir
+TMP_LOCALDIR=$(pwd)
+LQS_TEMPINSTALLDIR=".aptfast-setup"
+mkdir $LQS_TEMPINSTALLDIR
+cd $LQS_TEMPINSTALLDIR
 
 # Install apt-fast
 sudo apt update -y
-sudo apt install -y curl
+sudo apt install -y curl git
 sudo /bin/bash -c "$(curl -sL https://git.io/vokNn)"
-
-sudo apt-fast install -y git
 
 # Install apt-fast autocompletion
 git clone https://github.com/ilikenwf/apt-fast.git apt-fast
@@ -21,7 +19,7 @@ sudo chown root:root /etc/bash_completion.d/apt-fast
 . /etc/bash_completion
 
 # Remove temporary directory
-cd $tmp_localdir
-rm -rf $lqs_tempinstalldir
+cd $TMP_LOCALDIR
+rm -rf $LQS_TEMPINSTALLDIR
 
 echo "Done!"
