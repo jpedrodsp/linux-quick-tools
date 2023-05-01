09a95880fdd10d58979aa31b8f6c9629fac14531
João Pedro Patrocinio <jpedro9966@gmail.com>
2023-05-01 18:26:07 -0300

    Add script: install-systemc.sh

---
+++ ubuntu/install-systemc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Cloning SystemC Repository..."
+SYSTEMC_REPO_URL="https://github.com/accellera-official/systemc.git"
+SYSTEMC_REPO_TAGVER="2.3.4"
+SYSTEMC_REPO_OUTDIR="/tmp/systemc"
+SYSTEMC_INSTALLDIR="/usr/local/systemc-$SYSTEMC_REPO_TAGVER"
+git clone --depth 1 --branch $SYSTEMC_REPO_TAGVER $SYSTEMC_REPO_URL $SYSTEMC_REPO_OUTDIR
+
+echo "Configuring SystemC Installation"
+cd $SYSTEMC_REPO_OUTDIR
+mkdir build
+cd build
+
+echo "Building and installing..."
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SYSTEMC_INSTALLDIR ..
+make -j`nproc`
+make check -j`nproc`
+make install
+
+echo "Cleaning installation..."
+cd ..
+rm -rf build
+
+cd
+rm -rf $SYSTEMC_REPO_OUTDIR
+
+echo "Done!"
