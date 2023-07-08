852bcf93aae09e72e27359ce449ba3a6551c7f98
João Pedro Patrocinio <jpedro9966@gmail.com>
2023-07-08 16:05:40 -0300

    Fix protonvpn-install.sh script ;

--- ubuntu/protonvpn-install.sh
+++ ubuntu/protonvpn-install.sh
@@ -3,16 +3,16 @@
 # Exit script if any command fails
 set -e
 
-REPODEBURL = "https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3_all.deb"
-LOCALREPONAME = "protonvpn-stable-release_1.0.3_all.deb"
+REPODEBURL="https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3_all.deb"
+LOCALREPONAME="protonvpn-stable-release_1.0.3_all.deb"
 
-APT_COMMAND = "apt"
+APT_COMMAND="apt"
 if [ -x "$(command -v apt-fast)" ]; then
-    APT_COMMAND = "apt-fast"
+    APT_COMMAND="apt-fast"
 fi
 
 # Install System Tray Icon dependencies for ProtonVPN
-PROTONVPN_APT_DEPENDENCIES = "gnome-shell-extension-appindicator gir1.2-appindicator3-0.1"
+PROTONVPN_APT_DEPENDENCIES="gnome-shell-extension-appindicator gir1.2-appindicator3-0.1"
 sudo apt update -y
 sudo apt install -y $PROTONVPN_APT_DEPENDENCIES
 
